6e64b9bd5a956d48801ba1b36904de9c5b69b84f
ycs-201807065 <qkrwlsxor956@gmail.com>
2023-01-04 13:46:12 +0900

    test

---
+++ TrainMFC/CTrain.h
@@ -0,0 +1,62 @@
+﻿#pragma once
+#include "afxdialogex.h"
+
+#define RAIL_NUM 13	// 레일 개수
+
+struct ThreadArg
+{
+	HWND hwnd;
+	int type;
+};
+
+struct HwndArg
+{
+	HWND hwnd;
+};
+
+// CTrain 대화 상자
+
+class CTrain : public CDialog
+{
+	DECLARE_DYNAMIC(CTrain)
+
+public:
+	CTrain(CWnd* pParent = nullptr);   // 표준 생성자입니다.
+
+	virtual ~CTrain();
+
+	// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MAINTRAIN };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedCreate();
+	afx_msg void OnBnClickedStart();
+	afx_msg void OnBnClickedStop();
+	afx_msg	void TimerFunction(int flag);
+	afx_msg	void TrainMove(int left, int top, int right, int bottom);
+	CRect trainSize;
+	BOOL startFlag;
+	
+
+
+	BITMAP bmpInfo;  //비트맵
+	CWinThread* m_thread_move[3];  //열차 쓰레드
+	ThreadArg arg1;
+	HwndArg hWndArg;
+
+	int trainCount; //열차갯수
+
+	afx_msg void OnPaint();
+	afx_msg BOOL OnEraseBkgnd(CDC* pDC);
+	//afx_msg UINT DrawObject(LPVOID param);
+	//afx_msg HBRUSH OnCtlColor(CDC* pDC, CWnd* pWnd, UINT nCtlColor);
+	CStatic testText1;
+	CStatic testText2;
+
+};
---
+++ TrainMFC/TrainMFCView.h
@@ -0,0 +1,49 @@
+﻿
+// TrainMFCView.h: CTrainMFCView 클래스의 인터페이스
+//
+
+#pragma once
+
+
+class CTrainMFCView : public CView
+{
+protected: // serialization에서만 만들어집니다.
+	CTrainMFCView() noexcept;
+	DECLARE_DYNCREATE(CTrainMFCView)
+
+// 특성입니다.
+public:
+	CTrainMFCDoc* GetDocument() const;
+
+// 작업입니다.
+public:
+
+// 재정의입니다.
+public:
+	virtual void OnDraw(CDC* pDC);  // 이 뷰를 그리기 위해 재정의되었습니다.
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 구현입니다.
+public:
+	virtual ~CTrainMFCView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 생성된 메시지 맵 함수
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // TrainMFCView.cpp의 디버그 버전
+inline CTrainMFCDoc* CTrainMFCView::GetDocument() const
+   { return reinterpret_cast<CTrainMFCDoc*>(m_pDocument); }
+#endif
+
