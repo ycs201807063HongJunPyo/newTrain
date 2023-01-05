b92e01192bec2e528cf175cc98d4852d830364e3
ycs201807063HongJunPyo <rus3768@gmail.com>
2023-01-05 18:00:39 +0900

    tt

--- TrainMFC/CTrain.cpp
+++ TrainMFC/CTrain.cpp
@@ -30,8 +30,7 @@ int subRailBottomRight[3] = { 410, 310, 210 };
 BOOL insCheck[RAIL_NUM] = { FALSE, };
 BOOL smallInsCheck[3] = { FALSE, };
 BOOL subInsCheck[3] = { FALSE, };
-//열차 출발/정지
-BOOL trainFlag;
+
 //열차 기본값
 int trainAreaFlag;
 
@@ -73,7 +72,6 @@ UINT ThreadMoveTrain(LPVOID param);
 void CTrain::OnBnClickedCreate()
 {
 	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
-	trainFlag = TRUE;
 
 	arg1.hwnd = this->m_hWnd;
 	trainAreaFlag = GetDlgItemInt(IDC_EDIT_LINE);
@@ -90,7 +88,10 @@ void CTrain::OnBnClickedCreate()
 
 void CTrain::OnBnClickedStart()
 {
-	trainFlag = TRUE;
+	for (int i = 0; i < trainCount; i++)
+	{
+		m_thread_move[i]->ResumeThread();
+	}
 
 	//SetTimer(TRAIN_TIMER, 50, NULL);
 }
@@ -98,8 +99,10 @@ void CTrain::OnBnClickedStart()
 
 void CTrain::OnBnClickedStop()
 {
-	trainFlag = FALSE;
-
+	for (int i = 0; i < trainCount; i++)
+	{
+		m_thread_move[i]->SuspendThread();
+	}
 
 	/*
 	KillTimer(TRAIN_TIMER);
@@ -186,7 +189,7 @@ UINT DrawObject(LPVOID param, int type)
 	int trainWidth = 30;	//열차 가로 길이
 	int trainHeight = 30;	//열차 세로 길이
 
-	int flag = 1;			//열차 방향
+	int flag;			//열차 방향
 
 	int posX = 0;			//초기 x위치
 	int posY = 0;			//초기 y위치
@@ -197,9 +200,16 @@ UINT DrawObject(LPVOID param, int type)
 	int lineSelect;
 	lineSelect = type;
 
+	int stationCount = 0; //현재 도착역
+	int subStationCount = 0; //정차이후 출발역
+	int safeStationCount = 1; //안전거리
+
+
 	if (lineSelect == 0) {
+		flag = 3;
 		trainSpeed = 850;  //열차 속도
 		rect = CRect(trainSpeed, 20, trainSpeed + 30, 50);
+		safeStationCount = 3;
 		arraySize = (sizeof(smallRailBottomLeft) / sizeof(*smallRailBottomLeft));
 		for (int i = 0; i < arraySize; i++) {
 			threadRailTopLeft[i] = smallRailTopLeft[i];
@@ -209,6 +219,7 @@ UINT DrawObject(LPVOID param, int type)
 		}
 	}
 	else if (lineSelect == 1) {
+		flag = 1;
 		trainSpeed = 0;  //열차 속도
 		rect = CRect(trainSpeed, 20, trainSpeed + 30, 50);
 		arraySize = (sizeof(railBottomLeft) / sizeof(*railBottomLeft));
@@ -220,6 +231,7 @@ UINT DrawObject(LPVOID param, int type)
 		}
 	}
 	else if (lineSelect == 2) {
+		flag = 4;
 		trainSpeed = 380;  //열차 속도
 		rect = CRect(860, trainSpeed, 860 + 30, trainSpeed + 30);
 		arraySize = (sizeof(subRailBottomLeft) / sizeof(*subRailBottomLeft));
@@ -235,258 +247,248 @@ UINT DrawObject(LPVOID param, int type)
 
 	dc.Attach(hdc);
 
-	int stationCount = 0; //현재 도착역
-	int subStationCount = 0; //정차이후 출발역
-	int safeStationCount = 1; //안전거리
+	CRect stationRect;		//현재역 영역
+	CRect subStationRect;	//이전역 영역
+
 	while (1) {
-		if (trainFlag == TRUE) {
-			Sleep(10);
-			//열차 움직임 조정
-			if (lineSelect == 0) {
-				//안전거리용 if문
-				if (insCheck[4] == FALSE && insCheck[5] == FALSE && insCheck[6] == FALSE) {
-					trainSpeed -= 10;
-				}
-				if (stationCount <= 2) {
-					//왼
-					rect = CRect(trainSpeed, 70, trainSpeed + 30, 70 + 30);
-					InvalidateRect(pArg->hwnd, rect, TRUE);
-					trainX = trainSpeed + 30;
-				}
+		Sleep(10);
+		//열차 속도 설정
+		if (insCheck[safeStationCount] == FALSE || lineSelect == 2) {
+			//오른(1), 아래(2) 플래그
+			if (flag <= 2) {
+				trainSpeed += 10;
 			}
-			else if (lineSelect == 1) {
-				//안전거리용 if문
-				if (insCheck[safeStationCount] == FALSE) {
-					if (stationCount <= 6) {
-						trainSpeed += 10;
-					}
-					else {
-						trainSpeed -= 10;
-					}
-				}
-				//열차 이동
-				if (stationCount <= 4) {
-					//오른
-					rect = CRect(trainSpeed, 20, trainSpeed + 30, 50);
-					InvalidateRect(pArg->hwnd, rect, TRUE);
-					trainX = trainSpeed + 30;
-				}
-				else if (stationCount <= 6) {
-					//아래
-					rect = CRect(trainX - 30, trainSpeed, trainX, 30 + trainSpeed);
-					InvalidateRect(pArg->hwnd, rect, TRUE);
-					trainY = 30 + trainSpeed;
-				}
-				else if (stationCount <= 10) {
-					//왼
-					rect = CRect(trainSpeed, trainY - 30, trainSpeed + 30, trainY);
-					InvalidateRect(pArg->hwnd, rect, TRUE);
-					trainX = trainSpeed + 30;
-
-				}
-				else if (stationCount <= 12) {
-					//위
-					rect = CRect(trainX - 30, trainSpeed, trainX, 30 + trainSpeed);
-					InvalidateRect(pArg->hwnd, rect, TRUE);
-				}
-
+			//왼(3), 위(4) 플래그
+			else if (flag <= 4) {
+				trainSpeed -= 10;
 			}
-			else if (lineSelect == 2) {
-				if (lineWhile == TRUE) {
-					trainSpeed -= 10;
-				}
-				else {
-					trainSpeed += 10;
-				}
-				if (stationCount <= 3) {
-					//위
-					rect = CRect(850, trainSpeed, 870 + 30, trainSpeed + 30);
-					InvalidateRect(pArg->hwnd, rect, TRUE);
-				}
-
+		}
+		//열차 움직임 조정
+		if (lineSelect == 0) {
+			rect = CRect(trainSpeed, 70, trainSpeed + 30, 70 + 30);
+			trainX = trainSpeed + 30;
+		}
+		else if (lineSelect == 1) {
+			//열차 이동
+			if (stationCount <= 4) {
+				//오른
+				rect = CRect(trainSpeed, 20, trainSpeed + 30, 50);
+				trainX = trainSpeed + 30;
 			}
-			//역 색칠하기
-			CBrush brush;
-			CBrush* oldBrush = dc.SelectObject(&brush);
+			else if (stationCount <= 6) {
+				//아래
+				rect = CRect(trainX - 30, trainSpeed, trainX, 30 + trainSpeed);
+				trainY = 30 + trainSpeed;
+			}
+			else if (stationCount <= 10) {
+				//왼
+				rect = CRect(trainSpeed, trainY - 30, trainSpeed + 30, trainY);
+				trainX = trainSpeed + 30;
+			}
+			else if (stationCount <= 12) {
+				//위
+				rect = CRect(trainX - 30, trainSpeed, trainX, 30 + trainSpeed);
+			}
+		}
+		else if (lineSelect == 2) {
+			rect = CRect(850, trainSpeed, 870 + 30, trainSpeed + 30);
+		}
+		InvalidateRect(pArg->hwnd, rect, TRUE);
+		//역 색칠하기
+		CBrush brush;
+		CBrush* oldBrush = dc.SelectObject(&brush);
 
-			CRect stationRect;		//현재역 영역
-			CRect subStationRect;	//이전역 영역
+		dc.SelectObject(oldBrush);
+		dc.Rectangle(rect);	//열차 그리기
 
-			dc.SelectObject(oldBrush);
-			dc.Rectangle(rect);	//열차 그리기
-
-			stationRect = CRect(threadRailTopLeft[stationCount], threadRailBottomLeft[stationCount], threadRailTopRight[stationCount], threadRailBottomRight[stationCount]);
-			subStationRect = CRect(threadRailTopLeft[subStationCount], threadRailBottomLeft[subStationCount], threadRailTopRight[subStationCount], threadRailBottomRight[subStationCount]);
+		stationRect = CRect(threadRailTopLeft[stationCount], threadRailBottomLeft[stationCount], threadRailTopRight[stationCount], threadRailBottomRight[stationCount]);
+		subStationRect = CRect(threadRailTopLeft[subStationCount], threadRailBottomLeft[subStationCount], threadRailTopRight[subStationCount], threadRailBottomRight[subStationCount]);
 
 
-			if (IntersectRect(tmpRect, rect, stationRect) && stationCount >= 0 && lineSelect == 1)
+		if (IntersectRect(tmpRect, rect, stationRect) && stationCount >= 0 && lineSelect == 1)
+		{
+			insCheck[stationCount] = TRUE;
+		}
+
+		if (lineWhile == TRUE) {
+			if (IntersectRect(tmpRect, rect, stationRect) && stationCount >= 0)
 			{
-				insCheck[stationCount] = TRUE;
-			}
 
-			if (lineWhile == TRUE) {
-				if (IntersectRect(tmpRect, rect, stationRect) && stationCount >= 0)
-				{
-					
-					//색칠 + 무효화
-					brush.CreateSolidBrush(RGB(0, 255, 0));       // 초록
-					oldBrush = dc.SelectObject(&brush);
-					dc.Rectangle(stationRect);
-					//이전 부분과 충돌이 있을경우에만 무효화 해주기
-					if (IntersectRect(tmpRect, rect, subStationRect) && stationCount >= 1) {
-						InvalidateRect(pArg->hwnd, CRect(threadRailTopLeft[stationCount - 1], threadRailBottomLeft[stationCount - 1], threadRailTopRight[stationCount - 1], threadRailBottomRight[stationCount - 1]), TRUE);
-					}
+				//색칠 + 무효화
+				brush.CreateSolidBrush(RGB(0, 255, 0));       // 초록
+				oldBrush = dc.SelectObject(&brush);
+				dc.Rectangle(stationRect);
+				//이전 부분과 충돌이 있을경우에만 무효화 해주기
+				if (IntersectRect(tmpRect, rect, subStationRect) && stationCount >= 1) {
+					InvalidateRect(pArg->hwnd, CRect(threadRailTopLeft[stationCount - 1], threadRailBottomLeft[stationCount - 1], threadRailTopRight[stationCount - 1], threadRailBottomRight[stationCount - 1]), TRUE);
 				}
-				else if (IntersectRect(tmpRect, rect, subStationRect) && subStationCount >= 0)
-				{
-					insCheck[subStationCount] = FALSE;
-					//색만 칠하기
-					brush.CreateSolidBrush(RGB(255, 0, 0));       // 빨강
-					oldBrush = dc.SelectObject(&brush);
-					dc.Rectangle(subStationRect);
+			}
+			else if (IntersectRect(tmpRect, rect, subStationRect) && subStationCount >= 0)
+			{
+				insCheck[subStationCount] = FALSE;
+				//색만 칠하기
+				brush.CreateSolidBrush(RGB(255, 0, 0));       // 빨강
+				oldBrush = dc.SelectObject(&brush);
+				dc.Rectangle(subStationRect);
 
-				}
 			}
-			//반복 노선 전용
-			else if (lineWhile == FALSE) {
-				if (IntersectRect(tmpRect, rect, stationRect) && stationCount >= 0)
-				{
-					//색칠 + 무효화
-					brush.CreateSolidBrush(RGB(0, 255, 0));       // 초록
-					oldBrush = dc.SelectObject(&brush);
-					dc.Rectangle(stationRect);
-					//이전 부분과 충돌이 있을경우에만 무효화 해주기
-					if (IntersectRect(tmpRect, rect, subStationRect) && stationCount >= 0) {
-						InvalidateRect(pArg->hwnd, CRect(threadRailTopLeft[subStationCount], threadRailBottomLeft[subStationCount], threadRailTopRight[subStationCount], threadRailBottomRight[subStationCount]), TRUE);
-					}
+		}
+		//반복 노선 전용
+		else if (lineWhile == FALSE) {
+			if (IntersectRect(tmpRect, rect, stationRect) && stationCount >= 0)
+			{
+				//색칠 + 무효화
+				brush.CreateSolidBrush(RGB(0, 255, 0));       // 초록
+				oldBrush = dc.SelectObject(&brush);
+				dc.Rectangle(stationRect);
+				//이전 부분과 충돌이 있을경우에만 무효화 해주기
+				if (IntersectRect(tmpRect, rect, subStationRect) && stationCount >= 0) {
+					InvalidateRect(pArg->hwnd, CRect(threadRailTopLeft[subStationCount], threadRailBottomLeft[subStationCount], threadRailTopRight[subStationCount], threadRailBottomRight[subStationCount]), TRUE);
+				}
 
-				}
-				else if (IntersectRect(tmpRect, rect, subStationRect) && subStationCount >= 0)
-				{
-					brush.CreateSolidBrush(RGB(255, 0, 0));       // 빨강
-					oldBrush = dc.SelectObject(&brush);
-					dc.Rectangle(threadRailTopLeft[subStationCount], threadRailBottomLeft[subStationCount], threadRailTopRight[subStationCount], threadRailBottomRight[subStationCount]);
+			}
+			else if (IntersectRect(tmpRect, rect, subStationRect) && subStationCount >= 0)
+			{
+				brush.CreateSolidBrush(RGB(255, 0, 0));       // 빨강
+				oldBrush = dc.SelectObject(&brush);
+				dc.Rectangle(threadRailTopLeft[subStationCount], threadRailBottomLeft[subStationCount], threadRailTopRight[subStationCount], threadRailBottomRight[subStationCount]);
+			}
+		}
+		//정차, 방향 조정하기
+		//2호선 외선
+		if (lineSelect == 0) {
+			//정차, 방향 조정
+			if (stationCount <= 1 && ((100 - (stationCount * 20)) + trainSpeed) == threadRailTopRight[stationCount]) {
+				//왼쪽
+				flag = 3;
+				subStationCount = stationCount;
+				safeStationCount++;
+				Sleep(500);
+				stationCount++;
+				//자연스러운 역 이어주기
+				if (stationCount == 2) {
+					threadRailTopRight[stationCount] = railTopRight[5];
 				}
 			}
-			//정차, 방향 조정하기
-			//2호선 외선
-			if (lineSelect == 0) {
-				//정차, 방향 조정
-				if (stationCount <= 1 && ((100 - (stationCount * 20)) + trainSpeed) == threadRailTopRight[stationCount]) {
-					subStationCount = stationCount;
-					safeStationCount++;
-					Sleep(500);
-					stationCount++;
-					//자연스러운 역 이어주기
-					if (stationCount == 2) {
-						threadRailTopRight[stationCount] = railTopRight[5];
+			else if (stationCount == 2 && (50 + trainSpeed) == threadRailTopRight[stationCount]) {
+				//아래쪽
+				flag = 2;
+				stationCount++;
+				if (stationCount == 3) {
+					arraySize = (sizeof(railBottomLeft) / sizeof(*railBottomLeft));
+					for (int i = 0; i < arraySize; i++) {
+						threadRailTopLeft[i] = railTopLeft[i];
+						threadRailTopRight[i] = railTopRight[i];
+						threadRailBottomLeft[i] = railBottomLeft[i];
+						threadRailBottomRight[i] = railBottomRight[i];
 					}
+					trainSpeed = 80;
+					lineSelect = 1;
+					trainX = 490;
+					subStationCount = 5;
+					stationCount = 6;
+					safeStationCount = 6;
+					insCheck[stationCount] = TRUE;
 				}
-				else if (stationCount == 2 && (50 + trainSpeed) == threadRailTopRight[stationCount]) {
-					stationCount++;
-					if (stationCount == 3) {
-						arraySize = (sizeof(railBottomLeft) / sizeof(*railBottomLeft));
-						for (int i = 0; i < arraySize; i++) {
-							threadRailTopLeft[i] = railTopLeft[i];
-							threadRailTopRight[i] = railTopRight[i];
-							threadRailBottomLeft[i] = railBottomLeft[i];
-							threadRailBottomRight[i] = railBottomRight[i];
-						}
-						trainSpeed = 80;
-						lineSelect = 1;
-						trainX = 490;
-						subStationCount = 5;
-						stationCount = 6;
-						safeStationCount = 7;
-					}
-					Sleep(500);
-				}
+				Sleep(500);
 			}
-			//2호선 내선
-			else if (lineSelect == 1) {
-				//정차, 방향 조정
-				if (stationCount <= 4 && (50 + trainSpeed) == threadRailTopRight[stationCount]) {
-					//오른쪽
-					flag = 1;
-					subStationCount = stationCount;
-					safeStationCount++;
-					Sleep(1000);
-					stationCount++;
-					if (stationCount == 5) {
-						//아래쪽
-						flag = 2;
-						trainSpeed = 20;
-					}
-				}
-				else if ((stationCount >= 5 && stationCount <= 6 && (30 + trainSpeed) == threadRailBottomRight[stationCount] - 10)) {
+		}
+		//2호선 내선
+		else if (lineSelect == 1) {
+			//정차, 방향 조정
+			if (stationCount <= 4 && (50 + trainSpeed) == threadRailTopRight[stationCount]) {
+				//오른쪽
+				flag = 1;
+				subStationCount = stationCount;
+
+				Sleep(1000);
+				stationCount++;
+				safeStationCount = stationCount + 1;
+				if (stationCount == 5) {
 					//아래쪽
 					flag = 2;
-					subStationCount = stationCount;
-					safeStationCount++;
-					Sleep(1000);
-					stationCount++;
-					if (stationCount == 7) {
-						//왼쪽
-						flag = 3;
-						trainSpeed = trainX - 30;
-					}
-				}
-				else if (stationCount >= 5 && stationCount <= 10 && (70 + trainSpeed) == threadRailTopRight[stationCount]) {
-					//왼쪽
-					flag = 3;
-					subStationCount = stationCount;
-					safeStationCount++;
-					
-					Sleep(1000);
-					stationCount++;
-					if (stationCount == 11) {
-						//위쪽
-						flag = 4;
-						trainSpeed = trainY - 30;
-					}
-				}
-				else if ((stationCount >= 11 && stationCount <= 12 && (30 + trainSpeed) == threadRailBottomRight[stationCount] - 10)) {
-					//위쪽
-					flag = 4;
-					subStationCount = stationCount;
-					safeStationCount++;
-					if (safeStationCount == 13) {
-						safeStationCount = 0;
-					}
-					Sleep(1000);
-					stationCount++;
-					if (stationCount == 13) {
-						//오른쪽
-						flag = 1;
-						stationCount = 1;
-						safeStationCount = 2;
-						trainSpeed = 50;
-						trainX = 0;
-						trainY = 0;
-					}
+					trainSpeed = 20;
 				}
 			}
-			//반복
-			else if (lineSelect == 2) {
-				//정차, 방향 조정
-				if ((trainSpeed + 70) == threadRailBottomRight[stationCount]) {
-					subStationCount = stationCount;
-					Sleep(500);
-					if (lineWhile == TRUE) {
-						stationCount++;
-						if (stationCount == 3) {
-							subStationCount = 2;
-							stationCount = 1;
-							lineWhile = FALSE;
-						}
+			else if ((stationCount >= 5 && stationCount <= 6 && (30 + trainSpeed) == threadRailBottomRight[stationCount] - 10)) {
+				//아래쪽
+				flag = 2;
+				subStationCount = stationCount;
+
+				Sleep(1000);
+				stationCount++;
+				safeStationCount = stationCount + 1;
+				if (stationCount == 7) {
+					//왼쪽
+					flag = 3;
+					trainSpeed = trainX - 30;
+				}
+			}
+			else if (stationCount >= 5 && stationCount <= 10 && (70 + trainSpeed) == threadRailTopRight[stationCount]) {
+				//왼쪽
+				flag = 3;
+				subStationCount = stationCount;
+
+				Sleep(1000);
+				stationCount++;
+				safeStationCount = stationCount + 1;
+				if (stationCount == 11) {
+					//위쪽
+					flag = 4;
+					trainSpeed = trainY - 30;
+				}
+			}
+			else if ((stationCount >= 11 && stationCount <= 12 && (30 + trainSpeed) == threadRailBottomRight[stationCount] - 10)) {
+				//위쪽
+				flag = 4;
+				subStationCount = stationCount;
+
+
+				Sleep(1000);
+				stationCount++;
+				safeStationCount = stationCount + 1;
+				if (safeStationCount == 13) {
+					safeStationCount = 0;
+				}
+				if (stationCount == 13) {
+					//오른쪽
+					flag = 1;
+					stationCount = 1;
+					safeStationCount = 2;
+					trainSpeed = 50;
+					trainX = 0;
+					trainY = 0;
+				}
+			}
+		}
+		//반복
+		else if (lineSelect == 2) {
+			//정차, 방향 조정
+			if ((trainSpeed + 70) == threadRailBottomRight[stationCount]) {
+				subStationCount = stationCount;
+				Sleep(500);
+				if (lineWhile == TRUE) {
+					//위쪽
+					flag = 4;
+					stationCount++;
+					if (stationCount == 3) {
+						//아래쪽
+						flag = 2;
+						subStationCount = 2;
+						stationCount = 1;
+						lineWhile = FALSE;
 					}
-					else if (lineWhile == FALSE) {
-						stationCount--;
-						if (stationCount == -1) {
-							lineWhile = TRUE;
-							subStationCount = 0;
-							stationCount = 1;
-						}
+				}
+				else if (lineWhile == FALSE) {
+					//아래쪽
+					flag = 2;
+					stationCount--;
+					if (stationCount == -1) {
+						//위쪽
+						flag = 4;
+						lineWhile = TRUE;
+						subStationCount = 0;
+						stationCount = 1;
 					}
 				}
 			}
